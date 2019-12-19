b7ec1c1d48c0f1cfe51c67ca53b1378564bcc285
gek6 <thamiti@163.com>
2019-12-19 19:29:27 +0800

    Update article.controller.ts

--- src/article/article.controller.ts
+++ src/article/article.controller.ts
@@ -5,21 +5,9 @@ export class ArticleController {
     @Get()
     @Render("index")
     list(@Request() request,@Response() response){
-        console.log(request.url)
-        // 设置cookie
-        response.cookie("name",'zhangsan',{maxAge: 900000, httpOnly: true});
         return {
             name:'lane',
             age:'27'
         }
-    }
-    @Get(':id')
-    detail(@Request() request){
-        // 获取客户端cookie
-        let cookie =  request.cookies
-        console.log(cookie)
-
-    }
-
-    
+    } 
 }
